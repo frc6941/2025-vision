550b30af7d660a892157673ef901c78d237efa41
hs150521 <hs150521@163.com>
2024-10-16 19:53:44 +0800

    [Fix] Timestamp

--- output/NTTable.py
+++ output/NTTable.py
@@ -6,12 +6,16 @@ from Config.Config import Config
 
 
 class NTTable:
+    i = 0
     ntcore.NetworkTableInstance.getDefault().setServer(Config.NetworkConfig.ServerIP)
     ntcore.NetworkTableInstance.getDefault().startClient4(Config.NetworkConfig.DeviceName)
     timeStampPublisher: ntcore.FloatPublisher
     nt_table = ntcore.NetworkTableInstance.getDefault().getTable(
         "/" + Config.NetworkConfig.DeviceName + "/output")
     timeStampPublisher = nt_table.getFloatTopic("timestamp").publish()
+    timeStampPublisher2 = nt_table.getFloatTopic("timestamp2").publish()
 
     def periodic(self):
         self.timeStampPublisher.set(time.time())
+        self.timeStampPublisher2.set(self.i)
+        self.i += 1
