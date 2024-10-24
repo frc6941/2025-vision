5fd79ab10c02bc3ea2e4514e048401e9016da737
hs150521 <hs150521@163.com>
2024-10-24 18:18:02 +0800

    [FIXING] sync only DO NOT USE IT

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import sys
 import time
-from multiprocessing import cpu_count, Pool, Manager
+from multiprocessing import cpu_count
 from typing import Union
 
 import ntcore
@@ -22,94 +22,123 @@ from pipeline.PoseEstimator import SquareTargetPoseEstimator
 DEMO_ID = 29
 
 
-def get_image_observations(q_in, q_out):
+def imgProcesser(qImage: multiprocessing.Queue, qTime: multiprocessing.Queue, qConfig: multiprocessing.Queue,
+                 fps_count):
+    output_publisher: OutputPublisher = NTOutputPublisher()
+    stream_server = MjpegServer()
+    fiducial_detector = ArucoFiducialDetector(cv2.aruco.DICT_APRILTAG_36h11)
+    # estimator
+    camera_pose_estimator = MultiTargetCameraPoseEstimator()
+    tag_pose_estimator = SquareTargetPoseEstimator()
     while True:
-        if not q_in.empty():
-            image = q_in.get()
-            config = ConfigStore(LocalConfig(), RemoteConfig())
-            img = ArucoFiducialDetector(cv2.aruco.DICT_APRILTAG_36h11).detect_fiducials(image, config)
-            q_out.put(img)
+        if not qImage.empty():
+            image = qImage.get()
+            pTime = qTime.get()
+            pConfig = qConfig.get()
+            image_observations = fiducial_detector.detect_fiducials(image, pConfig)
+            [overlay_image_observation(image, x) for x in image_observations]
+            camera_pose_observation = camera_pose_estimator.solve_camera_pose(
+                [x for x in image_observations if x.tag_id != DEMO_ID], pConfig)
+            demo_image_observations = [x for x in image_observations if x.tag_id == DEMO_ID]
+            demo_pose_observation: Union[FiducialPoseObservation, None] = None
+            if len(demo_image_observations) > 0:
+                demo_pose_observation = tag_pose_estimator.solve_fiducial_pose(demo_image_observations[0], pConfig)
+            output_publisher.send(pConfig, pTime, camera_pose_observation, demo_pose_observation, fps_count.value)
+            stream_server.set_frame(image)
+
+
+def imgPublisher(qImage: multiprocessing.Queue, qTime: multiprocessing.Queue, qConfig: multiprocessing.Queue,
+                 fps_count):
+    capture = DefaultCapture()
+    config = ConfigStore(LocalConfig(), RemoteConfig())
+    local_config_source: ConfigSource = FileConfigSource()
+    remote_config_source: ConfigSource = NTConfigSource()
+    while True:
+        # update config
+        remote_config_source.update(config)
+
+        # get image
+        success, image = capture.get_frame(config)
+        if not success:
+            print("Failed to get image")
+            time.sleep(0.5)
+
+        # publish image with timestamp & config if processes aren't working
+        elif qImage.empty():
+            qImage.put(image)
+            qTime.put(time.time())
+            qConfig.put(config)
+            fps_count.value += 1
+        print(qImage.empty())
 
 
 if __name__ == "__main__":
+    capture = DefaultCapture()
+
     # multiprocessing to speed up
-    pool = Pool(cpu_count())
-    queueIn: multiprocessing.Queue = Manager().Queue(maxsize=cpu_count)
-    queueOut = Manager().Queue(maxsize=cpu_count)
-    for i in range(cpu_count()):
-        pool.apply_async(func=get_image_observations, args=(queueIn, queueOut))
+    manager = multiprocessing.Manager()
+    pool = multiprocessing.Pool(cpu_count() - 1)
+
+    # variables sharing between processes
+    queue_image = manager.Queue()
+    queue_time = manager.Queue()
+    queue_config = manager.Queue()
+    fps_count = manager.Value('i', 0)
+
+    # create cpu_count() process
+    for i in range(cpu_count() - 1):
+        pool.apply_async(func=imgProcesser, args=(queue_image, queue_time, queue_config, fps_count))
+    pool.apply_async(func=imgPublisher, args=(queue_image, queue_time, queue_config, fps_count))
 
     config = ConfigStore(LocalConfig(), RemoteConfig())
     local_config_source: ConfigSource = FileConfigSource()
     remote_config_source: ConfigSource = NTConfigSource()
     calibration_command_source: CalibrationCommandSource = NTCalibrationCommandSource()
 
-    # capture = GStreamerCapture()  # linux only
-    capture = DefaultCapture()  # other platforms
-    fiducial_detector = ArucoFiducialDetector(cv2.aruco.DICT_APRILTAG_36h11)
-    camera_pose_estimator = MultiTargetCameraPoseEstimator()
-    tag_pose_estimator = SquareTargetPoseEstimator()
-    output_publisher: OutputPublisher = NTOutputPublisher()
-    stream_server = MjpegServer()
+    # calibration setting
     calibration_session = CalibrationSession()
 
+    # start NT4
     local_config_source.update(config)
     ntcore.NetworkTableInstance.getDefault().setServer(config.local_config.server_ip)
     ntcore.NetworkTableInstance.getDefault().startClient4(config.local_config.device_id)
+
+    # start stream server
+    stream_server = MjpegServer()
     stream_server.start(config)
 
-    frame_count = 0
     last_print = 0
-    was_calibrating = False
-    while True:
-        remote_config_source.update(config)
-        timestamp = time.time()
-        success, image = capture.get_frame(config)
-        if not success:
-            time.sleep(0.5)
-            continue
 
-        fps = None
-        frame_count += 1
+    while True:
+        # update config
+        remote_config_source.update(config)
+
+        # print fps
         if time.time() - last_print > 1:
             last_print = time.time()
-            fps = frame_count
-            print("Running at", frame_count, "fps")
-            frame_count = 0
+            print("Running at", fps_count.value, "fps")
+            fps_count.value = 0
 
-        if calibration_command_source.get_calibrating(config):
-            # Calibration mode
-            was_calibrating = True
-            calibration_session.process_frame(image, calibration_command_source.get_capture_flag(config))
-
-        elif was_calibrating:
-            # Finish calibration
-            calibration_session.finish()
-            sys.exit(0)
-
-        elif config.local_config.has_calibration:
-            # Normal mode
-            while not queueIn.empty():
+        # check if calibrating
+        while calibration_command_source.get_calibrating(config):
+            # terminate processes to get image in main process
+            pool.terminate()
+            while True:
+                # get image
                 success, image = capture.get_frame(config)
-                if not success:
+                while not success:
+                    print("Failed to get image")
                     time.sleep(0.5)
-                    continue
-                queueIn.put(image)
-            while not queueOut.empty():
-                image_observations = queueOut.get()
-                [overlay_image_observation(image, x) for x in image_observations]
-                camera_pose_observation = camera_pose_estimator.solve_camera_pose(
-                    [x for x in image_observations if x.tag_id != DEMO_ID], config)
-                demo_image_observations = [x for x in image_observations if x.tag_id == DEMO_ID]
-                demo_pose_observation: Union[FiducialPoseObservation, None] = None
-                if len(demo_image_observations) > 0:
-                    demo_pose_observation = tag_pose_estimator.solve_fiducial_pose(demo_image_observations[0], config)
-                output_publisher.send(config, timestamp, camera_pose_observation, demo_pose_observation, fps)
+                    success, image = capture.get_frame(config)
 
-        else:
-            # No calibration
+                # calib
+                calibration_session.process_frame(image, calibration_command_source.get_capture_flag(config))
+
+                # Finish calibration
+                if not calibration_command_source.get_calibrating(config):
+                    calibration_session.finish()
+                    sys.exit(0)
+
+        if not config.local_config.has_calibration:
             print("No calibration found")
             time.sleep(0.5)
-
-        # image = cv2.undistort(image, config.local_config.camera_matrix, config.local_config.distortion_coefficients)
-        stream_server.set_frame(image)
