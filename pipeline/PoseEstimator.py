4ed4092c4c11ee5d4c0c639e59bb159ee4d8bdcf
hs150521 <hs150521@163.com>
2024-11-28 21:01:36 +0800

    [Feat] pnp

--- pipeline/PoseEstimator.py
+++ pipeline/PoseEstimator.py
@@ -2,17 +2,18 @@ from typing import Union
 
 import cv2
 import numpy
-from config.config import ConfigStore
-from vision_types import FiducialImageObservation, FiducialPoseObservation
 
+from config.config import ConfigStore
 from pipeline.coordinate_systems import openCvPoseToWpilib
+from vision_types import FiducialImageObservation, FiducialPoseObservation
 
 
 class PoseEstimator:
     def __init__(self) -> None:
         raise NotImplementedError
 
-    def solve_fiducial_pose(self, image_observation: FiducialImageObservation, config_store: ConfigStore) -> Union[FiducialPoseObservation, None]:
+    def solve_fiducial_pose(self, image_observation: FiducialImageObservation, config_store: ConfigStore) -> Union[
+        FiducialPoseObservation, None]:
         raise NotImplementedError
 
 
@@ -20,7 +21,8 @@ class SquareTargetPoseEstimator(PoseEstimator):
     def __init__(self) -> None:
         pass
 
-    def solve_fiducial_pose(self, image_observation: FiducialImageObservation, config_store: ConfigStore) -> Union[FiducialPoseObservation, None]:
+    def solve_fiducial_pose(self, image_observation: FiducialImageObservation, config_store: ConfigStore) -> Union[
+        FiducialPoseObservation, None]:
         fid_size = config_store.remote_config.fiducial_size_m
         object_points = numpy.array([[-fid_size / 2.0, fid_size / 2.0, 0.0],
                                      [fid_size / 2.0, fid_size / 2.0, 0.0],
@@ -28,8 +30,15 @@ class SquareTargetPoseEstimator(PoseEstimator):
                                      [-fid_size / 2.0, -fid_size / 2.0, 0.0]])
 
         try:
-            _, rvecs, tvecs, errors = cv2.solvePnPGeneric(object_points, image_observation.corners, config_store.local_config.camera_matrix,
-                                                          config_store.local_config.distortion_coefficients, flags=cv2.SOLVEPNP_IPPE_SQUARE)
+            # _, rvecs, tvecs, errors = cv2.solvePnPGeneric(object_points, image_observation.corners, config_store.local_config.camera_matrix,
+            #                                              config_store.local_config.distortion_coefficients, flags=cv2.SOLVEPNP_IPPE_SQUARE)
+
+            ## PnPRansac Ver.
+            _, rvecs, tvecs, errors = cv2.solvePnPRansac(object_points, image_observation.corners,
+                                                         config_store.local_config.camera_matrix,
+                                                         config_store.local_config.distortion_coefficients,
+                                                         iterationsCount=30, flags=cv2.SOLVEPNP_IPPE_SQUARE)
+
         except:
             return None
         return FiducialPoseObservation(
