d230f5fc82bdbc134b9c9bee62637a6b6717f528
RealAllenDa <2153867950@qq.com>
2025-01-13 20:43:51 +0800

    [Refactor] accelerate inter-process communication
    Instead of using queues for everything, use SharedMemory for sharing pictures and corresponding timestamps and use shared values for others.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import cProfile
 import math
 import multiprocessing
-import pickle
+import pstats
 import sys
+import threading
 import time
 from multiprocessing import cpu_count
+from multiprocessing.managers import SharedMemoryManager
+from multiprocessing.shared_memory import SharedMemory
 from typing import Optional
 
 import ntcore
@@ -14,13 +18,12 @@ from calibration.CalibrationCommandSource import (
 )
 from calibration.CalibrationSession import CalibrationSession
 from config.ConfigSource import ConfigSource, FileConfigSource, NTConfigSource
-from config.config import LocalConfig, RemoteConfig, ConfigStore
+from config.config import ConfigStore
 from output.DetectResult import DetectResult
 from output.StreamServer import MjpegServer
 from output.overlay_util import *
 from pipeline.CameraPoseEstimator import MultiTargetCameraPoseEstimator
 from pipeline.Capture import DefaultCapture
-# from pipeline.Capture import GStreamerCapture
 from pipeline.FiducialDetector import ArucoFiducialDetector
 from pipeline.PoseEstimator import SquareTargetPoseEstimator
 from vision_types import FiducialPoseObservation, CameraPoseObservation
@@ -28,30 +31,41 @@ from vision_types import FiducialPoseObservation, CameraPoseObservation
 DEMO_ID = 29
 
 
+def print_profile(pr: cProfile.Profile):
+    pstats.Stats(pr).sort_stats(pstats.SortKey.CUMULATIVE).print_stats()
+
+
 def process_img(
-        q_time,
-        q_config,
-        q_result,
-        q_detection,
-        fps,
+        e_ready: threading.Event,
+        m_time: SharedMemory,
+        s_config: multiprocessing.Value,
+        e_config: threading.Event,
+        q_detection: multiprocessing.Queue,
+        s_fps: multiprocessing.Value,
+        m_pic: SharedMemory
 ):
     fiducial_detector = ArucoFiducialDetector(cv2.aruco.DICT_APRILTAG_36h11)
     # estimator
     camera_pose_estimator = MultiTargetCameraPoseEstimator()
     tag_pose_estimator = SquareTargetPoseEstimator()
+    cfg = ConfigStore.model_validate_json(s_config.value)
+    shape = (cfg.remote_config.camera_resolution_height,
+             cfg.remote_config.camera_resolution_width,
+             3)
+    timestamp = numpy.ndarray((1,), dtype=numpy.float64, buffer=m_time.buf)
+    # noinspection PyTypeChecker
+    image: cv2.Mat = numpy.ndarray(shape, dtype=numpy.uint8, buffer=m_pic.buf)
+    print("[+] process_img: initialized. waiting for ready signal")
+    e_ready.wait()
     while True:
-        if not q_config.empty():
-            file = open('./tmp.pkl', 'rb')
-            image = pickle.load(file)
-            file.close()
-            p_time = q_time.get()
-            p_config = q_config.get()
-            time1 = time.time()
-            image_observations = fiducial_detector.detect_fiducials(image, p_config)
-            print("2 " + str(time.time() - time1))
+        try:
+            if e_config.is_set():
+                print("[.] process_img: syncing config")
+                cfg = ConfigStore.model_validate_json(s_config.value)
+            image_observations = fiducial_detector.detect_fiducials(image, cfg)
             [overlay_image_observation(image, x) for x in image_observations]
             camera_pose_observation = camera_pose_estimator.solve_camera_pose(
-                [x for x in image_observations if x.tag_id != DEMO_ID], p_config
+                [x for x in image_observations if x.tag_id != DEMO_ID], cfg
             )
             demo_image_observations = [
                 x for x in image_observations if x.tag_id == DEMO_ID
@@ -59,37 +73,44 @@ def process_img(
             demo_pose_observation: Optional[FiducialPoseObservation] = None
             if len(demo_image_observations) > 0:
                 demo_pose_observation = tag_pose_estimator.solve_fiducial_pose(
-                    demo_image_observations[0], p_config
+                    demo_image_observations[0], cfg
                 )
             send(
                 q_detection=q_detection,
-                timestamp=p_time,
+                timestamp=float(timestamp[0]),
                 observation=camera_pose_observation,
                 demo_observation=demo_pose_observation,
-                fps=fps.value,
+                fps=s_fps.value,
             )
-            q_result.put(image)
-            print("all " + str(time.time() - time1))
-            fps.value += 1
+            s_fps.value += 1
+        except Exception as e:
+            print(e)
 
 
-def streaming(q_result):
-    config_store = ConfigStore(LocalConfig(), RemoteConfig())
+def streaming(e_ready: threading.Event, m_pic: SharedMemory, s_config: multiprocessing.Value):
+    cfg = ConfigStore.model_validate_json(s_config.value)
+    shape = (cfg.remote_config.camera_resolution_height,
+             cfg.remote_config.camera_resolution_width,
+             3)
     # start stream server
     stream_server = MjpegServer()
-    stream_server.start(config_store)
+    stream_server.start(cfg)
     # show 1 frame every display_freq frames
     # TODO: remote config
     cnt = 0
     display_freq = 5
+    # noinspection PyTypeChecker
+    image: cv2.Mat = numpy.ndarray(shape, dtype=numpy.uint8, buffer=m_pic.buf)
+    print("[+] streaming: initialized. waiting for ready signal")
+    e_ready.wait()
     while True:
-        if not q_result.empty():
+        try:
             cnt += 1
             if cnt % display_freq == 0:
-                stream_server.set_frame(q_result.get())
+                stream_server.set_frame(image)
                 cnt = 0
-            else:
-                q_result.get()
+        except Exception as e:
+            print(e)
 
 
 def send(
@@ -170,36 +191,51 @@ def send(
     q_detection.put(DetectResult(fps, observation_data, demo_observation_data, math.floor(timestamp * 1000000)))
 
 
-def publish_img(q_time, q_config):
+def publish_img(
+        e_ready: threading.Event,
+        m_time: SharedMemory,
+        s_config: multiprocessing.Value,
+        e_config: threading.Event,
+        m_pic: SharedMemory):
     capture = DefaultCapture()
-    # capture = GStreamerCapture()
-    config_store = ConfigStore(LocalConfig(), RemoteConfig())
-    config_source_remote: ConfigSource = NTConfigSource()
-    config_source_local: ConfigSource = FileConfigSource()
-    config_source_local.update(config_store)
-    ntcore.NetworkTableInstance.getDefault().setServer(config_store.local_config.server_ip)
-    ntcore.NetworkTableInstance.getDefault().startClient4(config_store.local_config.device_id)
+    cfg = ConfigStore.model_validate_json(s_config.value)
+    shape = (cfg.remote_config.camera_resolution_height,
+             cfg.remote_config.camera_resolution_width,
+             3)
+    timestamp = numpy.ndarray((1,), dtype=numpy.float64, buffer=m_time.buf)
+    img_arr = numpy.ndarray(shape, dtype=numpy.uint8, buffer=m_pic.buf)
+    print("[+] publish_img: initialized. waiting for camera to be ready")
+    # get one frame until success
+    success, image = capture.get_frame(cfg)
+    while not success:
+        print("[-] publish_img: failed to get image")
+        time.sleep(0.5)
+        success, image = capture.get_frame(cfg)
+
+    print("[+] publish_img: camera is ready")
+    e_ready.set()
     while True:
-        # update config
-        config_source_remote.update(config_store)
+        try:
+            if e_config.is_set():
+                print("[.] publish_img: syncing config")
+                cfg = ConfigStore.model_validate_json(s_config.value)
+            # get image
+            success, image = capture.get_frame(cfg)
+            while not success:
+                print("[-] publish_img: failed to get image")
+                time.sleep(0.5)
+                success, image = capture.get_frame(cfg)
 
-        # get image
-        success, image = capture.get_frame(config_store)
-        while not success:
-            print("Failed to get image")
-            time.sleep(0.5)
-            success, image = capture.get_frame(config_store)
-
-        if q_config.empty():
-            file = open('./tmp.pkl', 'wb')
-            # noinspection PyTypeChecker
-            pickle.dump(image, file)
-            file.close()
-            q_time.put(time.time())
-            q_config.put(config_store)
+            img_arr[:] = image[:]
+            timestamp[0] = time.time()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
+    # initialize shared memory
+    smm = SharedMemoryManager()
+    smm.start()
 
     # multiprocessing to speed up
     manager = multiprocessing.Manager()
@@ -208,41 +244,55 @@ if __name__ == "__main__":
     pool3 = multiprocessing.Pool(processes=1)
 
     # variables sharing between processes
-    queue_image = manager.Queue()
-    queue_time = manager.Queue()
-    queue_config = manager.Queue()
-    queue_result = manager.Queue()
+    # TODO: Remove queue_detection?
     queue_detection = manager.Queue()
-    fps_count = manager.Value("i", 0)
+
+    # config initialization
+    config = ConfigStore()
+    local_config_source: ConfigSource = FileConfigSource()
+    remote_config_source: ConfigSource = NTConfigSource()
+    calibration_command_source: CalibrationCommandSource = NTCalibrationCommandSource()
+
+    # start NT4
+    local_config_source.update(config)
+    ntcore.NetworkTableInstance.getDefault().setServer(config.local_config.server_ip)
+    ntcore.NetworkTableInstance.getDefault().startClient4(config.local_config.device_id)
+
+    prev_config = config.model_dump_json()
+
+    # shared memory between processes
+    pic_sm = smm.SharedMemory(config.remote_config.camera_resolution_width *
+                              config.remote_config.camera_resolution_height * 3)
+    time_sm = smm.SharedMemory(1)
+
+    # shared state between processes
+    config_ss = manager.Value("W", "")
+    config_ss.value = config.model_dump_json()
+    config_se = manager.Event()
+    fps_ss = manager.Value("i", 0)
+
+    ready_se = manager.Event()
 
     # create cpu_count() process
     for i in range(cpu_count() - 3):  # TODO: change range when commit
         pool.apply_async(
             func=process_img,
             args=(
-                queue_time,
-                queue_config,
-                queue_result,
+                ready_se,
+                time_sm,
+                config_ss,
+                config_se,
                 queue_detection,
-                fps_count,
+                fps_ss,
+                pic_sm,
             ),
         )
-    pool2.apply_async(func=streaming, args=(queue_result,), )
-    pool3.apply_async(func=publish_img, args=(queue_time, queue_config,), )
-
-    config = ConfigStore(LocalConfig(), RemoteConfig())
-    local_config_source: ConfigSource = FileConfigSource()
-    remote_config_source: ConfigSource = NTConfigSource()
-    calibration_command_source: CalibrationCommandSource = NTCalibrationCommandSource()
+    pool2.apply_async(func=streaming, args=(ready_se, pic_sm, config_ss), )
+    pool3.apply_async(func=publish_img, args=(ready_se, time_sm, config_ss, config_se, pic_sm), )
 
     # calibration setting
     calibration_session = CalibrationSession()
     calibrating = False
-
-    # start NT4
-    local_config_source.update(config)
-    ntcore.NetworkTableInstance.getDefault().setServer(config.local_config.server_ip)
-    ntcore.NetworkTableInstance.getDefault().startClient4(config.local_config.device_id)
 
     # fps_counting
     last_print = 0
@@ -251,9 +301,6 @@ if __name__ == "__main__":
     _demo_observations_pub: ntcore.DoubleArrayPublisher
     _observations_pub: ntcore.DoubleArrayPublisher
     _fps_pub: ntcore.IntegerPublisher
-    config = ConfigStore(LocalConfig(), RemoteConfig())
-    ntcore.NetworkTableInstance.getDefault().setServer(config.local_config.server_ip)
-    ntcore.NetworkTableInstance.getDefault().startClient4(config.local_config.device_id)
     nt_table = ntcore.NetworkTableInstance.getDefault().getTable(
         "/" + config.local_config.device_id + "/output"
     )
@@ -269,15 +316,24 @@ if __name__ == "__main__":
     )
     _fps_pub = nt_table.getIntegerTopic("fps").publish()
 
+    print("[+] main: initialized. waiting for ready signal")
+    ready_se.wait()
     while True:
         # update config
         remote_config_source.update(config)
 
+        if prev_config != config.model_dump_json():
+            prev_config = config.model_dump_json()
+            config_ss.value = prev_config
+            config_se.set()
+        else:
+            config_se.clear()
+
         # print fps
         if time.time() - last_print > 1:
             last_print = time.time()
-            print("Running at", fps_count.value, "fps")
-            fps_count.value = 0
+            print("Running at", fps_ss.value, "fps")
+            fps_ss.value = 0
 
         if not queue_detection.empty():
             result: DetectResult = queue_detection.get()
